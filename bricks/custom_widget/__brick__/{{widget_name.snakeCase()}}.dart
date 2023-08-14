ea909aedf75987d88e400aeeb1633b9b0f67c69c
Elian Ortega <elian.mortega@gmail.com>
2023-08-14 15:41:53 -0600

    feat: finish create widget example

---
+++ bricks/custom_widget/__brick__/{{widget_name.snakeCase()}}.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class {{widget_name.pascalCase()}} extends StatelessWidget {
+  const {{widget_name.pascalCase()}}({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
